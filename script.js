39e4d32e64f0624458c81b1ef092246f69532cf1
Uday Raj Gupta <udayrajguptabth@gmail.com>
2026-03-02 17:23:00 +0530

    Updated design and sections

--- script.js
+++ script.js
@@ -332,3 +332,20 @@ document.head.appendChild(styleSheet);
   index = 1;
 
   setInterval(changeImage, 3000);
+document.querySelectorAll('.service-card').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.background = `
+      radial-gradient(circle at ${x}px ${y}px,
+      rgba(255,0,102,0.12),
+      rgba(255,255,255,0.65) 60%)
+    `;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.background = "rgba(255,255,255,0.65)";
+  });
+});
